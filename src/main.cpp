dce13a90a3360a7db5474d034c0be06b0b8da44b
Jamie Howell <69440188+Totally-A-Boar@users.noreply.github.com>
2025-06-20 19:05:42 -0400

    Take out GUI code and modify CMakeLists.txt
    Take out GUI code in Main.cpp and add comments to CMakeLists.txt (not finished)

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,9 @@
 #define FSB_REGISTRY_GET_FAILURE  0xFB000004 // Getting a registry value failed
 #define FSB_COMCTL_INIT_FAILURE   0xFB000005 // Initializing common controls failed
 #define FSB_CONSOLE_INIT_FAILURE  0xFB000006 // Error initializing the console
+#define FSB_NO_FOREGROUND_WINDOWS 0xFB000007 // FSB was launched with no foreground windows
 
 namespace fsb {
-// Structures
 //! Information used by the program belonging to a process's window.
 struct process_window {
     //! Process ID belonging to the target. Mainly used for display purposes
@@ -44,233 +44,7 @@ std::atomic<HANDLE> process_heap_handle;
 std::vector<process_window> windows;
 
 // Functions
-bool create_reg() {
-    HKEY key_handle;
-    LONG result = RegCreateKeyExW(HKEY_CURRENT_USER, FSB_REG_KEY.data(),
-        0, nullptr, REG_OPTION_NON_VOLATILE, KEY_WRITE,
-        nullptr, &key_handle, nullptr);
-
-    if (result != ERROR_SUCCESS) {
-        // Bypass the Clang-Tidy because these ancient function needs a C-Style array
-        wchar_t* buffer = nullptr;
-        (void)FormatMessageW(
-            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
-            FORMAT_MESSAGE_IGNORE_INSERTS,
-            nullptr, static_cast<DWORD>(result),
-            0, reinterpret_cast<LPWSTR>(&buffer), 0, nullptr);
-
-        const std::wstring description(buffer);
-        LocalFree(buffer);
-
-        std::wostringstream oss;
-        oss << L"An error occurred while trying to create the registry configuration for the " \
-               L"application!\r\n\r\n" \
-               L"Location: Line 71, fsb.exe (Main.cpp::create_reg)\r\n" \
-               L"Operation: Advapi32.dll!RegCreateKeyExW\r\n" \
-               L"Return value: " << result << L"\r\n" \
-               L"Error code: " << GetLastError() << L"\r\n" \
-               L"Description: " << description.c_str();
-
-        std::wcerr << oss.str();
-        MessageBoxW(nullptr, oss.str().c_str(), L"fsb - Error",
-            MB_OK | MB_ICONERROR);
-        return false;
-    }
-
-    DWORD value = 0;
-    result = RegSetValueExW(key_handle, L"gui_mode", 0, REG_DWORD,
-        reinterpret_cast<BYTE*>(&value), sizeof(DWORD));
-
-    if (result != ERROR_SUCCESS) {
-        // Bypass the Clang-Tidy because these ancient function needs a C-Style array
-        wchar_t* buffer = nullptr;
-        (void)FormatMessageW(
-            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
-            FORMAT_MESSAGE_IGNORE_INSERTS,
-            nullptr, static_cast<DWORD>(result),
-            0, reinterpret_cast<LPWSTR>(&buffer), 0, nullptr);
-
-        const std::wstring description(buffer);
-        LocalFree(buffer);
-
-        std::wostringstream oss;
-        oss << L"An error occurred while trying to create the registry configuration for the " \
-               L"application!\r\n\r\n" \
-               L"Location: Line 125, fsb.exe (Main.cpp::create_reg)\r\n" \
-               L"Operation: Advapi32.dll!RegSetValueExW\r\n" \
-               L"Return value: " << result << L"\r\n" \
-               L"Error code: " << GetLastError() << L"\r\n" \
-               L"Description: " << description.c_str();
-
-        std::wcerr << oss.str();
-        MessageBoxW(nullptr, oss.str().c_str(), L"fsb - Error",
-            MB_OK | MB_ICONERROR);
-        return false;
-    }
-
-    RegCloseKey(key_handle);
-    return true;
-}
-
-bool get_gui_mode() {
-    DWORD value = 0, type = 0, size = sizeof(DWORD);
-    LONG result = RegGetValueW(HKEY_CURRENT_USER, FSB_REG_KEY.data(), L"gui_mode",
-        RRF_RT_REG_DWORD, &type, &value, &size);
-
-    if (result != ERROR_SUCCESS || type != REG_DWORD) {
-        wchar_t* buffer = nullptr;
-        (void)FormatMessageW(
-            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
-            FORMAT_MESSAGE_IGNORE_INSERTS,
-            nullptr, static_cast<DWORD>(result),
-            0, reinterpret_cast<LPWSTR>(&buffer), 0, nullptr);
-
-        const std::wstring description(buffer);
-        LocalFree(buffer);
-
-        std::wostringstream oss;
-        oss << L"An error occurred while trying to retrieve the registry configuration for the " \
-               L"application!\r\n\r\n" \
-               L"Location: Line 111, fsb.exe (Main.cpp::get_gui_mode)\r\n" \
-               L"Operation: Advapi32.dll!RegGetValueW\r\n" \
-               L"Return value: " << result << L"\r\n" \
-               L"Error code: " << GetLastError() << L"\r\n" \
-               L"Description: " << description.c_str();
-
-        std::wcerr << oss.str();
-        MessageBoxW(nullptr, oss.str().c_str(), L"fsb - Error",
-            MB_OK | MB_ICONERROR);
-        exit(FSB_REGISTRY_GET_FAILURE);
-    }
-
-    return value != 0;
-}
-
-bool set_gui_mode(bool mode) {
-    HKEY key_handle;
-    DWORD value = mode;
-    LONG result = RegOpenKeyExW(HKEY_CURRENT_USER, FSB_REG_KEY.data(), 0,
-        KEY_WRITE, &key_handle);
-
-    if (result != ERROR_SUCCESS) {
-        wchar_t* buffer = nullptr;
-        (void)FormatMessageW(
-            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
-            FORMAT_MESSAGE_IGNORE_INSERTS,
-            nullptr, static_cast<DWORD>(result),
-            0, reinterpret_cast<LPWSTR>(&buffer), 0, nullptr);
-
-        const std::wstring description(buffer);
-        LocalFree(buffer);
-
-        std::wostringstream oss;
-        oss << L"An error occurred while trying to open the registry key for the " \
-               L"application!\r\n\r\n" \
-               L"Location: Line 145, fsb.exe (Main.cpp::set_gui_mode)\r\n" \
-               L"Operation: Advapi32.dll!RegOpenKeyExW\r\n" \
-               L"Return value: " << result << L"\r\n" \
-               L"Error code: " << GetLastError() << L"\r\n" \
-               L"Description: " << description.c_str();
-
-        std::wcerr << oss.str();
-        MessageBoxW(nullptr, oss.str().c_str(), L"fsb - Error",
-            MB_OK | MB_ICONERROR);
-        return false;
-    }
-
-    result = RegSetValueExW(key_handle, L"gui_mode", 0, REG_DWORD,
-        reinterpret_cast<BYTE*>(&value), sizeof(DWORD));
-
-    if (result != ERROR_SUCCESS) {
-        wchar_t* buffer = nullptr;
-        (void)FormatMessageW(
-            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
-            FORMAT_MESSAGE_IGNORE_INSERTS,
-            nullptr, static_cast<DWORD>(result),
-            0, reinterpret_cast<LPWSTR>(&buffer), 0, nullptr);
-
-        const std::wstring description(buffer);
-        LocalFree(buffer);
-
-        std::wostringstream oss;
-        oss << L"An error occurred while trying to set a registry key for the " \
-               L"application!\r\n\r\n" \
-               L"Location: Line 173, fsb.exe (Main.cpp::set_gui_mode)\r\n" \
-               L"Operation: Advapi32.dll!RegSetValueExW\r\n" \
-               L"Return value: " << result << L"\r\n" \
-               L"Error code: " << GetLastError() << L"\r\n" \
-               L"Description: " << description.c_str();
-
-        std::wcerr << oss.str();
-        MessageBoxW(nullptr, oss.str().c_str(), L"fsb - Error",
-            MB_OK | MB_ICONERROR);
-        return false;
-    }
-
-    RegCloseKey(key_handle);
-    return true;
-}
-
 void init_console() {
-    // Check if the app was called from a console
-    if (!GetConsoleWindow()) {
-        if (!AttachConsole(ATTACH_PARENT_PROCESS)) {
-            // If not, we allocate a new one
-            // If for some reason this function fails, it's most likely because of no resources
-            // being available, which means that the detailed error message most likely won't load,
-            // so show a short one and exit
-            if (!AllocConsole()) {
-                MessageBoxW(nullptr, L"AllocConsole error!", L"Error",
-                    MB_OK | MB_ICONERROR);
-                exit(FSB_CONSOLE_INIT_FAILURE);
-            }
-        }
-    }
-
-    // Open the console streams and check that the handles are valid
-    FILE* fp;
-
-    if (!_wfreopen_s(&fp, L"CONOUT$", L"w", stdout)) {
-        std::wostringstream oss;
-        oss << L"An error occurred while trying to initialize the console window.\r\n\r\n" \
-               L"Location: Line 238, fsb.exe (Main.cpp::init_console)\r\n" \
-               L"Operation: ucrtbase.dll!_wfreopen_s\r\n" \
-               L"Error code: " << errno << L"\r\n";
-
-        std::wcerr << oss.str();
-        MessageBoxW(nullptr, oss.str().c_str(), L"fsb - Error",
-            MB_OK | MB_ICONERROR);
-        exit(errno);
-    }
-
-    if (!_wfreopen_s(&fp, L"CONOUT$", L"w", stderr)) {
-        std::wostringstream oss;
-        oss << L"An error occurred while trying to initialize the console window.\r\n\r\n" \
-               L"Location: Line 251, fsb.exe (Main.cpp::init_console)\r\n" \
-               L"Operation: ucrtbase.dll!_wfreopen_s\r\n" \
-               L"Error code: " << errno << L"\r\n";
-
-        std::wcerr << oss.str();
-        MessageBoxW(nullptr, oss.str().c_str(), L"fsb - Error",
-            MB_OK | MB_ICONERROR);
-        exit(errno);
-    }
-
-    if (!_wfreopen_s(&fp, L"CONIN$", L"r", stdin)) {
-        std::wostringstream oss;
-        oss << L"An error occurred while trying to initialize the console window.\r\n\r\n" \
-               L"Location: Line 264, fsb.exe (Main.cpp::init_console)\r\n" \
-               L"Operation: ucrtbase.dll!_wfreopen_s\r\n" \
-               L"Error code: " << errno << L"\r\n";
-
-        std::wcerr << oss.str();
-        MessageBoxW(nullptr, oss.str().c_str(), L"fsb - Error",
-            MB_OK | MB_ICONERROR);
-        exit(errno);
-    }
-
-    std::ios::sync_with_stdio(true);
-
     // Casting to void because if these fail, it won't break the application
     // Unbuffered mode
     (void)setvbuf(stdout, nullptr, _IONBF, 0);
@@ -291,7 +65,8 @@ void init_console() {
     (void)_setmode(_fileno(stderr), _O_U8TEXT);
     (void)_setmode(_fileno(stdin), _O_U8TEXT);
 
-    // Since if the console doesn't allocate, the app exits, this should never fail, So cast to void
+    // Since if the console doesn't allocate, the app exits, this should never fail, So cast to
+    // void
     (void)SetConsoleTitleW(L"Full Screen Borderless");
 }
 
@@ -350,10 +125,13 @@ void show_console_menu() {
                 std::wcout << L"   ";
             }
 
-            std::wcout << windows[i].title << L" (Process Id: " << windows[i].process_id << ")\r\n";
+            std::wostringstream oss;
+            oss << windows[i].title << L" (Process Id: " << windows[i].process_id << ")\r\n";
+            std::wcout << oss.str();
         }
 
-        std::wcout << L"\r\nPress 'Q' or escape to quit.";
+        std::wcout << L"\r\nPress 'r' to refresh\r\n";
+        std::wcout << L"Press 'Q' or escape to quit.";
 
         wchar_t key = _getwch();
         if (key == 0x1B || key == L'Q' || key == L'q') {
@@ -362,6 +140,12 @@ void show_console_menu() {
         // Enter key
         if (key == 0x0D) {
             fullscreen_window(windows[selected].window_handle);
+            exit(0);
+        }
+        // R key
+        if (key == L'R' || key == L'r') {
+            windows.clear();
+            EnumWindows(EnumWindowsProc, 0);
         }
         // Up arrow
         if (key == 72) {
@@ -378,51 +162,25 @@ void show_console_menu() {
 
 } // namespace fsb
 
-int __stdcall wmain(int argc, wchar_t* argv[]) {
+int __stdcall wmain() {
     // Set the process handles on application initialization
     fsb::instance_handle.store(GetModuleHandleW(nullptr));
     fsb::process_handle.store(GetCurrentProcess());
     fsb::process_heap_handle.store(GetProcessHeap());
-
-    // RegCreateKeyExW doesn't fail if the key already exists, making it safe to always run at
-    // startup
-    if (!fsb::create_reg()) {
-        return FSB_REGISTRY_INIT_FAILURE;
-    }
-
-    for (int i = 0; i < argc; ++i) {
-        std::wstring arg = argv[i];
-        if (arg == L"--set-use-gui" || arg == L"-set-use-gui" || arg == L"/set-use-gui") {
-            if (!fsb::set_gui_mode(true)) {
-                return FSB_REGISTRY_SET_FAILURE;
-            }
-
-            std::wcout << L"Successfully set fsb GUI mode to ";
-            return 0;
-        }
-    }
 
     EnumWindows(fsb::EnumWindowsProc, 0);
     if (fsb::windows.empty()) {
         std::wostringstream oss;
         oss << L"You have launched fsb with no foreground windows! " \
                L"Please relaunch with at least one foreground window.";
+
+        std::wcerr << oss.str();
         MessageBoxW(nullptr, oss.str().c_str(), L"fsb - No foreground windows!",
             MB_OK | MB_ICONWARNING);
+        return FSB_NO_FOREGROUND_WINDOWS;
     }
 
-    if (fsb::get_gui_mode()) {
-        INITCOMMONCONTROLSEX iccex;
-        iccex.dwSize = sizeof(iccex);
-        iccex.dwICC = ICC_WIN95_CLASSES;
-        if (!InitCommonControlsEx(&iccex)) {
-            return FSB_COMCTL_INIT_FAILURE;
-        }
-    } else {
-        fsb::init_console();
-        fsb::show_console_menu();
-
-    }
+    fsb::init_console();
 
     CloseHandle(fsb::process_handle.load());
     return 0;
